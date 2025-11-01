6b5834a8e380a2324363a449fcf4ab1c4e44921a
Simeon Stix <93971902+MrS-E@users.noreply.github.com>
2025-11-01 09:01:03 +0100

    Install RPM Fusion and Steam on Fedora
    Added commands to install RPM Fusion and Steam.

--- setup_fedora.sh
+++ setup_fedora.sh
@@ -193,6 +193,12 @@ fi
 # echo "==> Enabling clamav service"
 # sudo systemctl enable --now clamav-freshclam.service
 
+echo "==> installing additional software"
+#Steam
+sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
+sudo dnf config-manager setopt fedora-cisco-openh264.enabled=1
+sudo dnf -y install steam
+
 echo
 echo "✅ All set for Fedora!"
 
