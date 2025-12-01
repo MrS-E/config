3952b538034d4f0fb621984fcfb69aaa1111d472
Simeon Stix <stixsim1@students.zhaw.ch>
2025-12-01 23:50:39 +0100

    create script for symlinks

---
+++ setup_symlinks.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+DIR="$(dirname "$(readlink -f "$0")")"
+
+ln -sfn $DIR/config/zshrc $HOME/.zshrc
+ln -sfn $DIR/config/vimrc $HOME/.vimrc
+ln -sfn $DIR/config/ssh $HOME/.ssh
+ln -sfn $DIR/.gitconfig $HOME/.gitconfig
+ln -sfn $DIR/config/gitconfig $HOME/.gitconfig
+ln -sfn $DIR/config/nvim $HOME/.config/nvim
+ln -sfn $DIR/config/nvim $HOME/.config/nvim
+ln -sfn $DIR/config/lazygit $HOME/.config/lazygit
+
